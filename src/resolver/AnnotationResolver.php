8ca840af744eaecfc021b57841747017434effee
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-06 14:29:22 +0300

    Refactored

--- src/resolver/AnnotationResolver.php
+++ src/resolver/AnnotationResolver.php
@@ -46,28 +46,50 @@ class AnnotationResolver extends Resolver
         $metadata->internalId = $identifier ?: uniqid();
         $metadata->name = $metadata->internalId;
 
+        $this->resolveClassAnnotations($classData, $metadata);
+
+        /** @var \ReflectionMethod $method */
+        foreach ($classData->getMethods() as $method) {
+            $this->resolveMethodAnnotation($method, $metadata);
+        }
+
+        return $metadata;
+    }
+
+    /**
+     * Resolve all class annotations.
+     *
+     * @param \ReflectionClass $classData
+     * @param ClassMetadata    $metadata
+     */
+    protected function resolveClassAnnotations(\ReflectionClass $classData, ClassMetadata &$metadata)
+    {
         /** @var MetadataInterface $annotation Read class annotations */
         foreach ($this->reader->getClassAnnotations($classData) as $annotation) {
             if (class_implements($annotation, MetadataInterface::class)) {
                 $annotation->toMetadata($metadata);
             }
         }
+    }
 
-        /** @var \ReflectionMethod $method */
-        foreach ($classData->getMethods() as $method) {
-            $methodAnnotations = $this->reader->getMethodAnnotations($method);
-            $methodMetadata = new MethodMetadata();
-            $methodMetadata->name = $method->getName();
-            $methodMetadata->modifiers = $method->getModifiers();
-            $methodMetadata->parameters = $method->getParameters();
+    /**
+     * Resolve all method annotations.
+     *
+     * @param \ReflectionMethod $method
+     * @param ClassMetadata     $metadata
+     */
+    protected function resolveMethodAnnotation(\ReflectionMethod $method, ClassMetadata &$metadata)
+    {
+        $methodAnnotations = $this->reader->getMethodAnnotations($method);
+        $methodMetadata = new MethodMetadata();
+        $methodMetadata->name = $method->getName();
+        $methodMetadata->modifiers = $method->getModifiers();
+        $methodMetadata->parameters = $method->getParameters();
 
-            /** @var MethodAnnotation $methodAnnotation */
-            foreach ($methodAnnotations as $methodAnnotation) {
-                $methodMetadata->options[$methodAnnotation->getMethodAlias()] = $methodAnnotation->convertToMetadata();
-            }
-            $metadata->methodsMetadata[$method->getName()] = $methodMetadata;
+        /** @var MethodAnnotation $methodAnnotation */
+        foreach ($methodAnnotations as $methodAnnotation) {
+            $methodMetadata->options[$methodAnnotation->getMethodAlias()] = $methodAnnotation->convertToMetadata();
         }
-
-        return $metadata;
+        $metadata->methodsMetadata[$method->getName()] = $methodMetadata;
     }
 }
