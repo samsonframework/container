b256cc2a8083362c6ef163b1abd807ad5af54ef1
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-14 16:05:01 +0300

    Added ConfiguratorInterface

---
+++ src/configurator/ConfiguratorInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 14.08.16 at 16:04
+ */
+namespace samsonframework\container\configurator;
+
+/**
+ * Dependency inject container configuration interface.
+ *
+ * @package samsonframework\container\configurator
+ */
+interface ConfiguratorInterface
+{
+
+}
