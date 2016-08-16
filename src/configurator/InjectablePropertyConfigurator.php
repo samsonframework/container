7e68596af60809513714162b1fd55744fb0e9f00
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-16 14:02:41 +0300

    Added InjectableProperty

---
+++ src/configurator/InjectablePropertyConfigurator.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 14.08.16 at 20:33
+ */
+namespace samsonframework\container\configurator;
+
+use samsonframework\container\configurator\expection\ClassDoesNotExists;
+use samsonframework\container\configurator\expection\TypeHintDoesNotExists;
+use samsonframework\container\metadata\PropertyMetadata;
+
+/**
+ * Property injection configurator.
+ *
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ */
+class InjectablePropertyConfigurator implements PropertyConfiguratorInterface
+{
+    const EXCEPTION_NO_TYPE_HINT = 'Cannot configure property %s injection - type hint not specified';
+
+    const EXCEPTION_NO_TYPE_HINT = 'Cannot configure property %s injection - type hint not specified';
+    /**
+     * Convert to class property metadata.
+     *
+     * @param PropertyMetadata $propertyMetadata Input metadata
+     *
+     * @return PropertyMetadata Annotation conversion to metadata
+     *
+     * @throws \Exception
+     */
+    public function toPropertyMetadata(PropertyMetadata $propertyMetadata)
+    {
+        // Check if there is no type hint - we cannot inject without it
+        if ($propertyMetadata->typeHint === null || $propertyMetadata->typeHint === '') {
+            throw new TypeHintDoesNotExists('Cannot configure property "' . $propertyMetadata->name . '" injection');
+        }
+
+        // Check if specified type hint exists
+        if (!class_exists($propertyMetadata->typeHint)) {
+            throw new ClassDoesNotExists('Cannot configure property "' . $propertyMetadata->name . '" with "' . $propertyMetadata->typeHint . '"');
+        }
+    }
+}
---
+++ src/configurator/exception/TypeHintDoesNotExists.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 16.08.16 at 13:53
+ */
+namespace samsonframework\container\configurator\expection;
+
+/**
+ * Type hint does not exists exception.
+ *
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ */
+class TypeHintDoesNotExists extends \InvalidArgumentException
+{
+
+}
