c01810a24c00b5eadba63c83f93e935fe279f91c
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-16 15:03:18 +0300

    Refactored InjectArgument annotation

--- src/annotation/InjectArgument.php
+++ src/annotation/InjectArgument.php
@@ -1,26 +1,22 @@
-<?php
-declare(strict_types = 1);
-
+<?php declare(strict_types = 1);
 /**
  * Created by Vitaly Iegorov <egorov@samsonos.com>.
  * on 07.08.16 at 15:46
  */
 namespace samsonframework\container\annotation;
 
-use samsonframework\container\metadata\MethodMetadata;
+use samsonframework\container\configurator\InjectableArgumentConfigurator;
 
 /**
  * Method argument injection annotation.
  *
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ *
  * @Annotation
  */
-class InjectArgument extends AnnotationWithValue implements MethodInterface
+class InjectArgument extends InjectableArgumentConfigurator
 {
-    /** @var string Method argument name */
-    protected $argumentName;
-
-    /** @var string Method argument type */
-    protected $argumentType;
+    use AnnotationValueTrait;
 
     /**
      * InjectArgument constructor.
@@ -31,75 +27,10 @@ class InjectArgument extends AnnotationWithValue implements MethodInterface
      */
     public function __construct(array $valueOrValues)
     {
-        parent::__construct($valueOrValues);
+        // Parse argument injection data
+        list($argumentName, $argumentType) = each($this->parseAnnotationValue($valueOrValues));
 
-        if (count($valueOrValues)) {
-            list($this->argumentName, $this->argumentType) = each($valueOrValues);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     *
-     * @throws \InvalidArgumentException
-     */
-    public function toMethodMetadata(MethodMetadata $methodMetadata)
-    {
-        // Check for argument name input and validity
-        if (!$this->checkArgumentExists($this->argumentName, $methodMetadata)) {
-            throw new \InvalidArgumentException(
-                '@InjectArgument argument "'
-                . $methodMetadata->classMetadata->className . '::'
-                . $methodMetadata->name . ' '
-                . $this->argumentName . '" does not exists'
-            );
-        }
-
-        // Check for type input
-        if ($this->argumentType === null) {
-            throw new \InvalidArgumentException(
-                '@InjectArgument argument "'
-                . $methodMetadata->classMetadata->className . '::'
-                . $methodMetadata->name . ' '
-                . $this->argumentName . '" type not specified'
-            );
-        }
-
-        // Store dependency with fully qualified type name
-        $methodMetadata->dependencies[$this->argumentName] = $this->buildFullClassName(
-            $this->argumentType,
-            $methodMetadata->classMetadata->nameSpace
-        );
-    }
-
-    /**
-     * Check method argument existance.
-     *
-     * @param string         $argument
-     * @param MethodMetadata $methodMetadata
-     *
-     * @return bool True if @InjectArgument argument name is valid
-     */
-    protected function checkArgumentExists(string $argument, MethodMetadata $methodMetadata) : bool
-    {
-        return $argument !== null && array_key_exists($argument, $methodMetadata->parametersMetadata);
-    }
-
-    /**
-     * Build full class name.
-     *
-     * @param string $className Full or short class name
-     * @param string $namespace Name space
-     *
-     * @return string Full class name
-     */
-    protected function buildFullClassName($className, $namespace)
-    {
-        // Check if we need to append namespace to dependency
-        if ($className !== null && strpos($className, '\\') === false) {
-            return $namespace . '\\' . $className;
-        }
-
-        return $className;
+        // Pass to injectable argument configurator
+        parent::__construct($argumentName, $argumentType);
     }
 }
