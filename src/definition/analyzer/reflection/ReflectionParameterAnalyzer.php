392188173848a1212f5983d8da7cf95412e0ca44
Molodyko Ruslan <molodyko@samsonos>
2016-09-17 08:42:22 +0300

    Refactored parameter reflection analyzer

--- src/definition/analyzer/reflection/ReflectionParameterAnalyzer.php
+++ src/definition/analyzer/reflection/ReflectionParameterAnalyzer.php
@@ -13,6 +13,7 @@ use samsonframework\container\definition\ClassDefinition;
 use samsonframework\container\definition\exception\MethodDefinitionNotFoundException;
 use samsonframework\container\definition\exception\ParameterDefinitionAlreadyExistsException;
 use samsonframework\container\definition\exception\ParameterDefinitionNotFoundException;
+use samsonframework\container\definition\ParameterDefinition;
 use samsonframework\container\definition\reference\ClassReference;
 use samsonframework\container\definition\reference\CollectionReference;
 use samsonframework\container\definition\reference\ConstantReference;
@@ -46,41 +47,78 @@ class ReflectionParameterAnalyzer implements ParameterAnalyzerInterface
 
             // Define parameter definition if not exists
             $parameterDefinition = $methodDefinition->setupParameter($parameterName);
-
-            // Set parameter metadata
-            if ($reflectionParameter->isDefaultValueAvailable()) {
-                $parameterDefinition->setValue($reflectionParameter->getDefaultValue());
-            }
-            if ($reflectionParameter->getType()) {
-                $parameterDefinition->setTypeHint($reflectionParameter->getType());
-            }
-            $parameterDefinition->setIsOptional($reflectionParameter->isOptional());
+            $this->setReflectionMetadata($parameterDefinition, $reflectionParameter);
 
             $dependency = $parameterDefinition->getDependency();
             // If dependency was not set
+            // UndefinedReference is the default value of dependency which was not use before
             if (!$dependency || ($dependency instanceof UndefinedReference)) {
-                // If default value available
-                if ($reflectionParameter->isDefaultValueAvailable()) {
-                    // There is a constant
-                    if ($reflectionParameter->isDefaultValueConstant()) {
-                        $parameterDefinition->setDependency(
-                            new ConstantReference($reflectionParameter->getDefaultValueConstantName())
-                        );
-                    } else { // There is some php types
-                        $parameterDefinition->setDependency(
-                            CollectionReference::convertValueToReference($reflectionParameter->getDefaultValue())
-                        );
-                    }
-                    // There is class dependency
-                } elseif (
-                    is_object($parameterDefinition->getTypeHint()) &&
-                    (string)$parameterDefinition->getTypeHint() !== '' &&
-                    !$parameterDefinition->getTypeHint()->isBuiltin()
-                ) {
-                    $class = (string)$parameterDefinition->getTypeHint();
-                    $parameterDefinition->setDependency(new ClassReference($class));
-                }
+                $this->setDependencyByReflection($parameterDefinition, $reflectionParameter);
             }
         }
+    }
+
+    /**
+     * Set initial reflection metadata into the parameter definition
+     *
+     * @param ParameterDefinition $parameterDefinition
+     * @param \ReflectionParameter $reflectionParameter
+     */
+    public function setReflectionMetadata(
+        ParameterDefinition $parameterDefinition,
+        \ReflectionParameter $reflectionParameter
+    ) {
+        // Set parameter metadata
+        if ($reflectionParameter->isDefaultValueAvailable()) {
+            $parameterDefinition->setValue($reflectionParameter->getDefaultValue());
+        }
+        if ($reflectionParameter->getType()) {
+            $parameterDefinition->setTypeHint($reflectionParameter->getType());
+        }
+        $parameterDefinition->setIsOptional($reflectionParameter->isOptional());
+    }
+
+    /**
+     * Set dependency value by reflection
+     *
+     * @param ParameterDefinition $parameterDefinition
+     * @param \ReflectionParameter $reflectionParameter
+     * @throws \InvalidArgumentException
+     * @throws ReferenceNotImplementsException
+     */
+    public function setDependencyByReflection(
+        ParameterDefinition $parameterDefinition,
+        \ReflectionParameter $reflectionParameter
+    ) {
+        // If default value available
+        if ($reflectionParameter->isDefaultValueAvailable()) {
+            // There is a constant
+            if ($reflectionParameter->isDefaultValueConstant()) {
+                $parameterDefinition->setDependency(
+                    new ConstantReference($reflectionParameter->getDefaultValueConstantName())
+                );
+            } else { // There is some php types
+                $parameterDefinition->setDependency(
+                    CollectionReference::convertValueToReference($reflectionParameter->getDefaultValue())
+                );
+            }
+            // There is class dependency
+        } elseif ($this->checkIfTypeHingIsClass($parameterDefinition)) {
+            $class = (string)$parameterDefinition->getTypeHint();
+            $parameterDefinition->setDependency(new ClassReference($class));
+        }
+    }
+
+    /**
+     * Check if type hint can be as class reference dependency
+     *
+     * @param ParameterDefinition $parameterDefinition
+     * @return bool
+     */
+    protected function checkIfTypeHingIsClass(ParameterDefinition $parameterDefinition)
+    {
+        return is_object($parameterDefinition->getTypeHint()) &&
+            (string)$parameterDefinition->getTypeHint() !== '' &&
+            !$parameterDefinition->getTypeHint()->isBuiltin();
     }
 }
